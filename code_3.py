9632545edf9af7ff349ccd922884e7d106ad3921
genfilweb <142978168+genfilweb@users.noreply.github.com>
2024-03-30 22:15:52 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+
+    });
+}
+
+
+    return data;
+    const data = generateRandomData();
+}
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    data.forEach(item => {
+main();
+function generateRandomData() {
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int i = 0; i < 10; i++) {
+}
+        for (int item : data) {
+        }
+        }
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+
+public class RandomDataGenerator {
+    public static void main(String[] args) {
